361b9ccab905ca39eae4b99ce491fa93c8aa5049
Brahim Kaliochi <kaliochibrahim@gmail.com>
2026-01-21 17:12:20 +0000

    add put end point in routers

--- src/api/routes/tasks.routes.ts
+++ src/api/routes/tasks.routes.ts
@@ -1,5 +1,5 @@
 import {Router} from 'express'
-import { addTask, getTask, deleteTask} from '../../controller/task.controller';
+import { addTask, getTask, deleteTask,updateTask} from '../../controller/task.controller';
 import { validateParamsRequest, validateTaskRequest, validateUpdateRequest } from '../../middleware/tasks.middleware';
 
 const taskRouter = Router()
@@ -7,7 +7,7 @@ const taskRouter = Router()
 taskRouter.post("/",validateTaskRequest,addTask)
 taskRouter.get("/",getTask)
 taskRouter.delete("/:id", validateParamsRequest,deleteTask)
-taskRouter.put("/tasks_update",validateUpdateRequest)
+taskRouter.put("/tasks_update",validateUpdateRequest,updateTask)
 
 
 export default taskRouter;
